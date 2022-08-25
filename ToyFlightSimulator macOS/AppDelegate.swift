7b58b4fb0eb82c6b9841ffd9b6c49fe45c926ab6
Albertino Padin <albertino.padin@gmail.com>
2022-08-25 00:13:23 -0700

    Initial Commit

---
+++ ToyFlightSimulator macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  ToyFlightSimulator macOS
+//
+//  Created by Albertino Padin on 8/25/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
