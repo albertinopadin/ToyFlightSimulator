5f19169409eabb09bd2db4a41fd7061406508760
Albertino Padin <albertino.padin@gmail.com>
2023-01-08 10:36:04 -0800

    Implemented Sphere GameObject.

---
+++ ToyFlightSimulator Shared/GameObjects/Sphere.swift
@@ -0,0 +1,13 @@
+//
+//  Sphere.swift
+//  ToyFlightSimulator
+//
+//  Created by Albertino Padin on 1/8/23.
+//
+
+class Sphere: GameObject {
+    init() {
+        super.init(name: "Sphere", meshType: .Sphere)
+//        useBaseColorTexture(.None)
+    }
+}
