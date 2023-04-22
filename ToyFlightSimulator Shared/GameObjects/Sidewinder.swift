e29fef73420e3558ba0485c5fa6a09269883f75a
Albertino Padin <albertino.padin@gmail.com>
2023-04-21 19:41:36 -0700

    Refactored missile functionality into class, subclassing AIM-9 & AIM-120 from that.

--- ToyFlightSimulator Shared/GameObjects/Sidewinder.swift
+++ ToyFlightSimulator Shared/GameObjects/Sidewinder.swift
@@ -5,31 +5,8 @@
 //  Created by Albertino Padin on 4/5/23.
 //
 
-class Sidewinder: SubMeshGameObject {
-    var direction: float3 = float3(0, 0, 0)
-    var speed: Float = 0.0
-    
+class Sidewinder: Missile {
     init() {
         super.init(name: "Sidewinder", meshType: .F18_Sidewinder, renderPipelineStateType: .OpaqueMaterial)
-    }
-    
-    func fire(direction: float3, speed: Float) {
-        self.direction = direction
-        self.speed = speed
-    }
-    
-    override func doUpdate() {
-        super.doUpdate()
-        let currentPos = self.getPosition()
-        let delta = direction * speed
-        let newPos = currentPos + delta
-        
-        if abs(newPos.x) > 1000 || abs(newPos.y) > 1000 || abs(newPos.z) > 1000 {
-            // Reap from scene
-            print("Removing self {\(self.getName()), \(self.getID())} from scene.")
-            self.parent?.removeChild(self)
-        } else {
-            self.setPosition(newPos)
-        }
     }
 }
