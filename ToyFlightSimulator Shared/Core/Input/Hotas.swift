0c8dfd0e3beae6d07076fc4962c6a1025584675d
Albertino Padin <albertino.padin@gmail.com>
2023-06-08 02:09:51 -0700

    Very early implementation of reading the raw joystick data.

--- ToyFlightSimulator Shared/Core/Input/Hotas.swift
+++ ToyFlightSimulator Shared/Core/Input/Hotas.swift
@@ -3,7 +3,7 @@
 //  ToyFlightSimulator macOS
 //
 //  Created by Albertino Padin on 6/6/23.
-//
+//  Heavily inspired by https://github.com/Arti3DPlayer/USBDeviceSwift
 
 import IOKit.hid
 import Foundation
@@ -16,6 +16,8 @@ public extension Notification.Name {
 }
 
 class Hotas {
+    static let reportSize: Int = 64
+    
     var gameControllerDevice: IOHIDDevice?
 
     init() {
@@ -42,34 +44,63 @@ class Hotas {
     
     @objc func run() {
         print("[Hotas run]")
-        let manager = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
-//        let matchingDict = IOServiceMatching(kIOHIDDeviceKey)
+        let managerRef = IOHIDManagerCreate(kCFAllocatorDefault, IOOptionBits(kIOHIDOptionsTypeNone))
         let matchingDict: NSMutableDictionary = [kIOHIDVendorIDKey: NSNumber(value: 0x044f),
                                                  kIOHIDProductIDKey: NSNumber(value: 0x0402)]
 
 
-        IOHIDManagerSetDeviceMatching(manager, matchingDict)
-        IOHIDManagerScheduleWithRunLoop(manager, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue)
-        IOHIDManagerOpen(manager, IOOptionBits(kIOHIDOptionsTypeNone))
-        IOHIDManagerRegisterDeviceMatchingCallback(manager, Hotas.deviceMatchingCallback, nil)
-        IOHIDManagerRegisterDeviceRemovalCallback(manager, Hotas.deviceRemovalCallback, nil)
+        IOHIDManagerSetDeviceMatching(managerRef, matchingDict)
+        IOHIDManagerScheduleWithRunLoop(managerRef, CFRunLoopGetCurrent(), CFRunLoopMode.defaultMode.rawValue)
+        IOHIDManagerOpen(managerRef, IOOptionBits(kIOHIDOptionsTypeNone))
+        
+        let deviceMatchingCallback: IOHIDDeviceCallback = { inContext, inResult, inSender, inDeviceRef in
+            print("[deviceMatchingCallback]")
+            print("inContext: \(inContext), inResult: \(inResult), inSender: \(inSender), device ref: \(inDeviceRef)")
+            let this:Hotas = unsafeBitCast(inContext, to: Hotas.self)
+            this.deviceAdded(inResult, inSender: inSender!, inIOHIDDeviceRef: inDeviceRef)
+        }
+
+        let deviceRemovalCallback: IOHIDDeviceCallback = { inContext, inResult, inSender, inDeviceRef in
+            let this:Hotas = unsafeBitCast(inContext, to: Hotas.self)
+            this.deviceRemoved(inIOHIDDeviceRef: inDeviceRef)
+        }
+        
+        IOHIDManagerRegisterDeviceMatchingCallback(managerRef,
+                                                   deviceMatchingCallback,
+                                                   unsafeBitCast(self, to: UnsafeMutableRawPointer.self))
+        IOHIDManagerRegisterDeviceRemovalCallback(managerRef,
+                                                  deviceRemovalCallback,
+                                                  unsafeBitCast(self, to: UnsafeMutableRawPointer.self))
         
         RunLoop.current.run()
     }
-
-    static let deviceMatchingCallback: IOHIDDeviceCallback = { inContext, inResult, inSender, device in
-        print("[deviceMatchingCallback]")
-        print("inContext: \(inContext), inResult: \(inResult), inSender: \(inSender), device: \(device)")
-        guard let inContext else { return }
-        let controller = Unmanaged<Hotas>.fromOpaque(inContext).takeUnretainedValue()
-        controller.gameControllerDevice = device
-        print("[deviceMatchingCallback] USB Joystick detected!")
+    
+    func deviceAdded(_ inResult: IOReturn, inSender: UnsafeMutableRawPointer, inIOHIDDeviceRef: IOHIDDevice!) {
+        let report = UnsafeMutablePointer<UInt8>.allocate(capacity: Hotas.reportSize)
+        let inputCallback: IOHIDReportCallback = { inContext, inResult, inSender, inType, reportId, report, reportLength in
+            let this:Hotas = unsafeBitCast(inContext, to: Hotas.self)
+            this.read(inResult, inSender: inSender!, type: inType, reportId: reportId, report: report, reportLength: reportLength)
+        }
+        
+        IOHIDDeviceRegisterInputReportCallback(inIOHIDDeviceRef,
+                                               report,
+                                               Hotas.reportSize,
+                                               inputCallback,
+                                               unsafeBitCast(self, to: UnsafeMutableRawPointer.self))
     }
-
-    static let deviceRemovalCallback: IOHIDDeviceCallback = { inContext, inResult, inSender, device in
-        guard let inContext else { return }
-        let controller = Unmanaged<Hotas>.fromOpaque(inContext).takeUnretainedValue()
-        controller.gameControllerDevice = nil
-        print("[deviceRemovalCallback] USB Joystick removed!")
+    
+    func deviceRemoved(inIOHIDDeviceRef: IOHIDDevice!) {
+        print("[Hotas deviceRemoved]")
+    }
+    
+    func read(_ inResult: IOReturn,
+                     inSender: UnsafeMutableRawPointer,
+                     type: IOHIDReportType,
+                     reportId: UInt32,
+                     report: UnsafeMutablePointer<UInt8>,
+                     reportLength: CFIndex) {
+        let data = Data(bytes: UnsafePointer<UInt8>(report), count: reportLength)
+        // TODO: Make sense of this data:
+//        print("[Hotas read] data: \(data.base64EncodedString())")
     }
 }
