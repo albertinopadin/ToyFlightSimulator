653414ce572bf5d6d0419280c15d2c6db026e3ea
Albertino Padin <albertino.padin@gmail.com>
2023-08-03 21:34:39 -0700

    Added Unit Tests target.

---
+++ ToyFlightSimulatorTests/NodeTests.swift
@@ -0,0 +1,31 @@
+//
+//  NodeTests.swift
+//  ToyFlightSimulatorTests
+//
+//  Created by Albertino Padin on 8/3/23.
+//
+
+import XCTest
+@testable import ToyFlightSimulator
+
+final class NodeTests: XCTestCase {
+    
+    func testSuccessfulInit() {
+        let nodeName = "TestNode"
+        let node = Node(name: nodeName)
+        XCTAssertEqual(node.getName(), nodeName)
+        XCTAssertNotNil(node.getID())
+    }
+    
+    func testAddRemoveChild() {
+        let node = Node(name: "TestNode")
+        XCTAssertEqual(node.children.count, 0)
+        let child = Node(name: "ChildNode")
+        node.addChild(child)
+        XCTAssertEqual(node.children.count, 1)
+        let childFromArray = node.children.first(where: { $0.getID() == child.getID() })
+        XCTAssertNotNil(childFromArray)
+        node.removeChild(child)
+        XCTAssertEqual(node.children.count, 0)
+    }
+}
