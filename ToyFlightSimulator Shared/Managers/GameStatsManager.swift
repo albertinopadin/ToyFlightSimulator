9a17fc129fd81f5d65c56c86cbe36b95ae880224
Albertino Padin <albertino.padin@gmail.com>
2025-02-26 22:36:20 -0800

    Add memory footprint to Game Stats

--- ToyFlightSimulator Shared/Managers/GameStatsManager.swift
+++ ToyFlightSimulator Shared/Managers/GameStatsManager.swift
@@ -34,4 +34,37 @@ final class GameStatsManager: ObservableObject {
             frame = 0
         }
     }
+    
+    // TODO:
+//    public func threadInfo() {
+//        Thread.isMultiThreaded()
+//        ProcessInfo.processInfo.processName
+//        ProcessInfo.processInfo.processIdentifier
+//        ProcessInfo.processInfo.physicalMemory
+//    }
+    
+    // From: https://developer.apple.com/forums/thread/105088
+    private func rawMemoryFootprint() -> mach_vm_size_t? {
+        // The `TASK_VM_INFO_COUNT` and `TASK_VM_INFO_REV1_COUNT` macros are too
+        // complex for the Swift C importer, so we have to define them ourselves.
+        let TASK_VM_INFO_COUNT = mach_msg_type_number_t(MemoryLayout<task_vm_info_data_t>.size / MemoryLayout<integer_t>.size)
+        let TASK_VM_INFO_REV1_COUNT = mach_msg_type_number_t(MemoryLayout.offset(of: \task_vm_info_data_t.min_address)! / MemoryLayout<integer_t>.size)
+        var info = task_vm_info_data_t()
+        var count = TASK_VM_INFO_COUNT
+        let kr = withUnsafeMutablePointer(to: &info) { infoPtr in
+            infoPtr.withMemoryRebound(to: integer_t.self, capacity: Int(count)) { intPtr in
+                task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), intPtr, &count)
+            }
+        }
+        guard
+            kr == KERN_SUCCESS,
+            count >= TASK_VM_INFO_REV1_COUNT
+        else { return nil }
+        return info.phys_footprint
+    }
+    
+    public func memoryFootprint() -> String {
+        guard let memFootprint = rawMemoryFootprint() else { return 0.formatted(.byteCount(style: .memory)) }
+        return memFootprint.formatted(.byteCount(style: .memory))
+    }
 }
