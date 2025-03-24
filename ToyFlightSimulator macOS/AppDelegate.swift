523632fa23e30169e710e2496bb0fd0223a845f6
Albertino Padin <albertino.padin@gmail.com>
2025-03-23 19:01:14 -0700

    Add audio system, playing in its own thread

--- ToyFlightSimulator macOS/AppDelegate.swift
+++ ToyFlightSimulator macOS/AppDelegate.swift
@@ -10,20 +10,18 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        AudioManager.StopGameMusic()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-
 
 }
 
