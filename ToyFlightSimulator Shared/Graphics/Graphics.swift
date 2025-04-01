dfa04de20bae4b938926a3c62bce3c9b2bd8d77f
Albertino Padin <albertino.padin@gmail.com>
2025-03-31 21:45:44 -0700

    Use OSAllocatedUnfairLock to protect Graphics class properties

--- ToyFlightSimulator Shared/Graphics/Graphics.swift
+++ ToyFlightSimulator Shared/Graphics/Graphics.swift
@@ -5,27 +5,64 @@
 //  Created by Albertino Padin on 9/25/22.
 //
 
+import os
+
 class Graphics {
-    private static var _shaderLibrary: ShaderLibrary!
-    public static var Shaders: ShaderLibrary { return _shaderLibrary }
+    private static let shadersLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _shaderLibrary: ShaderLibrary!
+    public static var Shaders: ShaderLibrary {
+        return withLock(shadersLock) {
+            return _shaderLibrary
+        }
+    }
     
-    private static var _vertexDescriptorLibrary: VertexDescriptorLibrary!
-    public static var VertexDescriptors: VertexDescriptorLibrary { return _vertexDescriptorLibrary }
+    private static let vertexDescriptorsLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _vertexDescriptorLibrary: VertexDescriptorLibrary!
+    public static var VertexDescriptors: VertexDescriptorLibrary {
+        return withLock(vertexDescriptorsLock) {
+            return _vertexDescriptorLibrary
+        }
+    }
     
-    private static var _renderPipelineStateLibrary: RenderPipelineStateLibrary!
-    public static var RenderPipelineStates: RenderPipelineStateLibrary { return _renderPipelineStateLibrary }
+    private static let renderPipelineStatesLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _renderPipelineStateLibrary: RenderPipelineStateLibrary!
+    public static var RenderPipelineStates: RenderPipelineStateLibrary {
+        return withLock(renderPipelineStatesLock) {
+            return _renderPipelineStateLibrary
+        }
+    }
     
-    private static var _computePipelineStateLibrary: ComputePipelineStateLibrary!
-    public static var ComputePipelineStates: ComputePipelineStateLibrary { return _computePipelineStateLibrary }
+    private static let computePipelineStatesLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _computePipelineStateLibrary: ComputePipelineStateLibrary!
+    public static var ComputePipelineStates: ComputePipelineStateLibrary {
+        return withLock(computePipelineStatesLock) {
+            return _computePipelineStateLibrary
+        }
+    }
     
-    private static var _depthStencilStateLibrary: DepthStencilStateLibrary!
-    public static var DepthStencilStates: DepthStencilStateLibrary { return _depthStencilStateLibrary }
+    private static let depthStencilStatesLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _depthStencilStateLibrary: DepthStencilStateLibrary!
+    public static var DepthStencilStates: DepthStencilStateLibrary {
+        return withLock(depthStencilStatesLock) {
+            return _depthStencilStateLibrary
+        }
+    }
     
-    private static var _samplerStateLibrary: SamplerStateLibrary!
-    public static var SamplerStates: SamplerStateLibrary { return _samplerStateLibrary }
+    private static let samplerStatesLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _samplerStateLibrary: SamplerStateLibrary!
+    public static var SamplerStates: SamplerStateLibrary {
+        return withLock(samplerStatesLock) {
+            return _samplerStateLibrary
+        }
+    }
     
-    private static var _mdlVertexDescriptorLibrary: MDLVertexDescriptorLibrary!
-    public static var MDLVertexDescriptors: MDLVertexDescriptorLibrary { return _mdlVertexDescriptorLibrary }
+    private static let mdlVertexDescriptorsLock = OSAllocatedUnfairLock()
+    nonisolated(unsafe) private static var _mdlVertexDescriptorLibrary: MDLVertexDescriptorLibrary!
+    public static var MDLVertexDescriptors: MDLVertexDescriptorLibrary {
+        return withLock(mdlVertexDescriptorsLock) {
+            return _mdlVertexDescriptorLibrary
+        }
+    }
     
     public static func Initialize() {
         _shaderLibrary = ShaderLibrary()
